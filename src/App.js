85a957241a4c7758d2439a81b68aa4daaf9aad32
javad <javadbinabdullah@gmail.com>
2024-04-23 11:02:47 +0530

    changed navbar

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ function App() {
           <img src="./logo_p.png" width="50" height="50" class="d-inline-block" alt="" />
           Integration Ninja
         </a> */}
-        <h3>logo</h3>
+        <h3>changed logo</h3>
       </nav>
       <div className='container'>
         <div className="row">
